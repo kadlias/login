d610e2175b6aa305bb4560e9ee8d1c477a696267
Elias <110313311+kadlias@users.noreply.github.com>
2023-05-09 21:29:13 -0300

    initial comiit

---
+++ funcionalidades.js
@@ -0,0 +1,11 @@
+function logear() {
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("password").value;
+  
+    if (user === "Elias22" && pass === "123siempre") {
+      alert("Bienvenido nuevamente!");
+    } else {
+      alert("Datos incorrectos, intente nuevamente");
+    }
+  }
+  
